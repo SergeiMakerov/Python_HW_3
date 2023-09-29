dc3cb814c673eacf3ee17dd49ca5a92243987419
Sergei <makerovs@yandex.ru>
2023-09-29 22:32:08 +0700

    add 2 in task 1

--- Task1.py
+++ Task1.py
@@ -1,8 +1,13 @@
 my_list = [1, 2, 3, 1, 2, 4, 0, 5, 2]
-my_set = set()
-
-for i in my_list:
-    if my_list.count(i) > 1:
-        my_set.add(i)
+#my_set = set()
+UNIO_COUNT = 1
+# for i in my_list:
+#     if my_list.count(i) > UNIO_COUNT:
+#         my_set.add(i)
         
-print(list(my_set))
+# print(list(my_set))
+res = []
+for num in my_list:
+    if num not in res and my_list.count(num)> UNIO_COUNT:
+        res.append(num)
+print(res)
